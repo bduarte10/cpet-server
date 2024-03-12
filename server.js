0046b377b1d6cff05944f58a115e36a1a4b56827
Bruno <duartebruno2208@gmail.com>
2024-03-12 11:42:35 -0300

    feat: add toasts

--- server.js
+++ server.js
@@ -6,63 +6,47 @@ const mailgunTransport = require('nodemailer-mailgun-transport')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')))
-app.use(express.json())
+app.use(express.json()) // Adicione este middleware para analisar o corpo da solicitação como JSON
 
+// Configuração de transporte de e-mail com Mailgun
 const auth = {
   auth: {
     api_key: process.env.MAILGUN_API_KEY,
-    domain: 'sandbox9de6cef58bdb44bbb1213fee3df411aa.mailgun.org',
+    domain: 'sandbox9de6cef58bdb44bbb1213fee3df411aa.mailgun.org', // Altere para o seu domínio configurado no Mailgun
   },
 }
 
 const transporter = nodemailer.createTransport(mailgunTransport(auth))
 
+// Rota para lidar com o envio de e-mail
 app.post('/send-email', async function (req, res) {
-  const { celular, email, nome } = req.body
+  const { celular, email, nome } = req.body // Remove o await aqui, pois req.body não é uma promessa
 
-  // Primeiro e-mail para o proprietário
-  const ownerMailOptions = {
+  const mailOptions = {
     from: 'duartebruno2208@gmail.com',
-    to: 'duartebruno2208@gmail.com', // Seu e-mail
-    subject: 'Novo Lead CPET',
-    text: `Nome: ${nome}\nEmail: ${email}\nCelular: ${celular}`,
+    // to: 'deboraaustralia1@gmail.com',
+    to: 'duartebruno2208@gmail.com',
+    subject: 'Lead CPET',
+    text: `Nome: ${nome}\nEmail: ${email}\nCelular: ${celular}`, // Ajuste o formato do corpo do e-mail
   }
 
-  // Segundo e-mail para o lead
-  const leadMailOptions = {
-    from: 'conectaedubrasil@gmail.com',
-    to: email, // E-mail do lead
-    subject: 'Recebemos seu contato!',
-    text: `Olá ${nome},\n\nSuas informações foram recebidas com sucesso. Retornaremos o mais breve possível.`,
-  }
-
-  transporter.sendMail(ownerMailOptions, function (error, info) {
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      console.error('Erro ao enviar e-mail para proprietário:', error)
-      res.status(500).send('Erro ao enviar e-mail para proprietário')
+      console.error('Erro ao enviar e-mail:', error)
+      res.status(500).send('Erro ao enviar e-mail')
     } else {
-      console.log('E-mail enviado para proprietário:', info.response)
-
-      // Envia o segundo e-mail para o lead
-      transporter.sendMail(leadMailOptions, function (error, info) {
-        if (error) {
-          console.error('Erro ao enviar e-mail para lead:', error)
-          res.status(500).send('Erro ao enviar e-mail para lead')
-        } else {
-          console.log('E-mail enviado para lead:', info.response)
-          res.send('E-mails enviados com sucesso')
-        }
-      })
+      console.log('E-mail enviado:', info.response)
+      res.send('E-mail enviado com sucesso')
     }
   })
 })
 
 // Rota para servir a página React
-app.get('*', function (_, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
 })
